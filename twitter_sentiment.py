9faf9fcedf13a59ce14a7e5c1107ca37c8e48bb5
ZHANGCHENG <Lena@ZHANGCHENGs-MacBook-Pro.local>
2018-06-06 21:47:19 +0800

    I've added twitter sentiment project.

---
+++ twitter_sentiment.py
@@ -0,0 +1,8 @@
+import tweepy
+from textblob import textblob
+
+consumer_key = 'sVgDpjhKsv4kSUCMVt8YF1oo0'
+consumer_secret = '4UVbsnbi4574iKkp47zTxNmjv2Ucejuf3L8DPeEq2kD9jXD9y6'
+
+access_token = '1634743334-WUPiUYq1uH9vEqm3gJ6dv3CmhXQNNPy5UjE5mJR'
+access_token_secret = 'ypsPOLZGnkhFypKVG4OyL0kljsHE1Bvn2f90iEuD65R7j'
